1e64608530c5b1cc4589b350e6775cbd6d7f2922
Евгений Чертов <dumpy1@mail.ru>
2022-04-27 14:52:57 +0300

    теперь точно добавил

--- Lesson_01/index.js
+++ Lesson_01/index.js
@@ -1,6 +1,20 @@
-const goodsName = 'whatsminer';
-let goodsQuantity = 10;
-let goodsCategory = 'ASIC';
-let goodsPrice = 4876;
-let goodsCost = goodsPrice * goodsQuantity;
-console.log('goodsCost: ', goodsCost +' у.е.');
+{
+    const goodsName = 'whatsminer';
+    console.log('goodsName: ', goodsName);
+    let goodsQuantity = 10;
+    let goodsCategory = 'ASIC';
+    let goodsPrice = 4876;
+    let goodsCost = goodsPrice * goodsQuantity;
+    console.log('goodsCost: ', goodsCost + ' у.е.');
+}
+
+{
+    const goodsName = 'RX570';
+    console.log('goodsName: ', goodsName);
+    let goodsQuantity = 97;
+    let goodsCategory = 'GPU card';
+    let goodsPrice = 368;
+    let goodsCost = goodsPrice * goodsQuantity;
+    console.log('goodsCost: ', goodsCost + ' у.е.');
+
+}
