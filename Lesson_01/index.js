38c4405331d9cf55468dd1ab90eb57c4f17594b0
Евгений Чертов <dumpy1@mail.ru>
2022-04-27 15:08:24 +0300

    change all let -> const

--- Lesson_01/index.js
+++ Lesson_01/index.js
@@ -1,20 +1,20 @@
 {
     const goodsName = 'whatsminer';
     console.log('goodsName: ', goodsName);
-    let goodsQuantity = 10;
-    let goodsCategory = 'ASIC';
-    let goodsPrice = 4876;
-    let goodsCost = goodsPrice * goodsQuantity;
+    const goodsQuantity = 10;
+    const goodsCategory = 'ASIC';
+    const goodsPrice = 4876;
+    const goodsCost = goodsPrice * goodsQuantity;
     console.log('goodsCost: ', goodsCost + ' у.е.');
 }
 
 {
     const goodsName = 'RX570';
     console.log('goodsName: ', goodsName);
-    let goodsQuantity = 97;
-    let goodsCategory = 'GPU card';
-    let goodsPrice = 368;
-    let goodsCost = goodsPrice * goodsQuantity;
+    const goodsQuantity = 97;
+    const goodsCategory = 'GPU card';
+    const goodsPrice = 368;
+    const goodsCost = goodsPrice * goodsQuantity;
     console.log('goodsCost: ', goodsCost + ' у.е.');
 
 }
