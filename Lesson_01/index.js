2e88a34d95bb77404a455fa3a58702bf3ed9a396
Евгений Чертов <dumpy1@mail.ru>
2022-04-27 13:41:35 +0300

    initial commit

---
+++ Lesson_01/index.js
@@ -0,0 +1,6 @@
+const goodsName = 'whatsminer';
+let goodsQuantity = 10;
+let goodsCategory = 'ASIC';
+let goodsPrice = 4876;
+let goodsCost = goodsPrice * goodsQuantity;
+console.log('goodsCost: ', goodsCost +' у.е.');
