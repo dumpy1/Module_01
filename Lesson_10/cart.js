b1da45dce6eae5441f2c059a34e868376584dfcf
Martikot <martikot@iMac-Martikot.local>
2022-07-26 22:29:13 +0300

    Домашка 10го урока

---
+++ Lesson_10/cart.js
@@ -0,0 +1,54 @@
+const cart = {
+
+    items: [],
+    count: 0,
+    get TotalPrice() {
+        return this.calculateItemPrice();
+    },
+    add(item, price, count = 1) {
+        this.items.push({
+            item,
+            price,
+            count,
+        });
+
+        this.increaseCount(count);
+
+    },
+    increaseCount(quantity) {
+        this.count += quantity;
+    },
+    // calculateItemPrice(itemPrice = 0) {
+    //     acc = 0;
+    //     for (let i = 0; i < this.items.length; i++) {
+    //         itemPrice = this.items[i].price * this.items[i].count;
+    //         acc += itemPrice            
+    //     }   
+    //     return acc;
+    // },
+    calculateItemPrice() {
+        const sum = this.items.reduce((acc, currentItem) => {
+            itemPrice = currentItem.price * currentItem.count;
+            return acc + itemPrice;
+        },0)
+
+        return sum;
+    },
+    clear() {
+        this.items = [],
+            this.totalPrice = 0;
+        this.count = 0;
+    },
+    print() {
+        console.log(JSON.stringify(this.items));
+        console.log(`totalPrice:  ${this.TotalPrice}`);
+    },
+}
+
+
+cart.clear();
+cart.add('styka', 100, 3);
+cart.add('styka2', 10, 2);
+cart.add('styka2', 1);
+cart.add('styka2', 1000, 4);
+cart.print();
---
+++ Lesson_10/index.js
@@ -0,0 +1,10 @@
+console.log('uniqueCount');
+
+const uniqueCount = (arr) => {
+    const uniqueArr = Array.from(new Set(arr));
+    
+    return uniqueArr.length;
+}
+
+
+console.log('uniqCount(listIPv4): ', uniqueCount(listIPv4));
