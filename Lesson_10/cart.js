7b8444862cc5c5e6f52148b35a6181ddd068a5f6
Martikot <martikot@iMac-Martikot.local>
2022-07-26 23:51:58 +0300

    установил ЕСЛинтер

--- Lesson_10/cart.js
+++ Lesson_10/cart.js
@@ -1,49 +1,50 @@
+'use strict';
+
 const cart = {
 
-    items: [],
-    count: 0,
-    get TotalPrice() {
-        return this.calculateItemPrice();
-    },
-    add(item, price, count = 1) {
-        this.items.push({
-            item,
-            price,
-            count,
-        });
+  items: [],
+  count: 0,
+  get TotalPrice() {
+    return this.calculateItemPrice();
+  },
+  add(item, price, count = 1) {
+    this.items.push({
+      item,
+      price,
+      count,
+    });
 
-        this.increaseCount(count);
+    this.increaseCount(count);
+  },
+  increaseCount(quantity) {
+    this.count += quantity;
+  },
+  // calculateItemPrice(itemPrice = 0) {
+  //     acc = 0;
+  //     for (let i = 0; i < this.items.length; i++) {
+  //         itemPrice = this.items[i].price * this.items[i].count;
+  //         acc += itemPrice
+  //     }
+  //     return acc;
+  // },
+  calculateItemPrice() {
+    const sum = this.items.reduce((acc, currentItem) => {
+      const itemPrice = currentItem.price * currentItem.count;
+      return acc + itemPrice;
+    }, 0);
 
-    },
-    increaseCount(quantity) {
-        this.count += quantity;
-    },
-    // calculateItemPrice(itemPrice = 0) {
-    //     acc = 0;
-    //     for (let i = 0; i < this.items.length; i++) {
-    //         itemPrice = this.items[i].price * this.items[i].count;
-    //         acc += itemPrice            
-    //     }   
-    //     return acc;
-    // },
-    calculateItemPrice() {
-        const sum = this.items.reduce((acc, currentItem) => {
-            itemPrice = currentItem.price * currentItem.count;
-            return acc + itemPrice;
-        },0)
-
-        return sum;
-    },
-    clear() {
-        this.items = [],
-            this.totalPrice = 0;
-        this.count = 0;
-    },
-    print() {
-        console.log(JSON.stringify(this.items));
-        console.log(`totalPrice:  ${this.TotalPrice}`);
-    },
-}
+    return sum;
+  },
+  clear() {
+    this.items = [],
+    this.totalPrice = 0;
+    this.count = 0;
+  },
+  print() {
+    console.log(JSON.stringify(this.items));
+    console.log(`totalPrice:  ${this.TotalPrice}`);
+  },
+};
 
 
 cart.clear();
--- Lesson_10/index.js
+++ Lesson_10/index.js
@@ -1,10 +1,12 @@
+'use strict';
+
 console.log('uniqueCount');
 
 const uniqueCount = (arr) => {
-    const uniqueArr = Array.from(new Set(arr));
-    
-    return uniqueArr.length;
-}
+  const uniqueArr = Array.from(new Set(arr));
+
+  return uniqueArr.length;
+};
 
 
 console.log('uniqCount(listIPv4): ', uniqueCount(listIPv4));
